57f559f0f92641ef0f15772ac612fd0b6d429f3c
Maxime Gagnon <m.gagnon941@gmail.com>
2022-10-09 16:15:18 -0400

    style: Create base page layout
    Signed-off-by: 2156153 <m.gagnon941@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ArtistListComponent } from './artist-list/artist-list.component';
-import { ArtistDetailsComponent } from './artist-details/artist-details.component';
-import { ArtistAlbumsComponent } from './artist-albums/artist-albums.component';
-import { ArtistConcertsComponent } from './artist-details/artist-concerts/artist-concerts.component';
-import { ArtistAlbumTracksComponent } from './artist-albums/artist-album-tracks/artist-album-tracks.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRippleModule } from '@angular/material/core';
+
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ArtistListComponent } from './components/artist-list/artist-list.component';
+import { ArtistDetailsComponent } from './components/artist-details/artist-details.component';
+import { ArtistAlbumsComponent } from './components/artist-details/artist-albums/artist-albums.component';
+import { ArtistAlbumTracksComponent } from './components/artist-details/artist-albums/artist-album-tracks/artist-album-tracks.component';
+import { ArtistConcertsComponent } from './components/artist-details/artist-concerts/artist-concerts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ArtistListComponent,
-    ArtistDetailsComponent,
-    ArtistAlbumsComponent,
-    ArtistConcertsComponent,
-    ArtistAlbumTracksComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CardComponent,
+		ArtistListComponent,
+		ArtistDetailsComponent,
+		ArtistAlbumsComponent,
+		ArtistAlbumTracksComponent,
+		ArtistConcertsComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+
+		MatButtonModule,
+		MatCardModule,
+		MatInputModule,
+		MatTabsModule,
+		MatSnackBarModule,
+		MatIconModule,
+		MatDividerModule,
+		MatRippleModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.sass']
+})
+export class CardComponent implements OnInit {
+	@Input() canRemove: boolean = false;
+	@Input() model: any = {}
+	@Output() remove = new EventEmitter<string>();
+
+	constructor() { }
+	ngOnInit(): void { }
+}
