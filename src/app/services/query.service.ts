397c8b9211b94ad580be613d2e2d4b944f1ba23d
Maxime Gagnon <m.gagnon941@gmail.com>
2022-10-10 11:17:17 -0400

    feat: Implement QueryService
    Signed-off-by: 2156153 <m.gagnon941@gmail.com>

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SpotifyService } from './spotify.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class QueryService {
+interface IQueryService {
+	queryArtist(name: string): Promise<Observable<any> | undefined>
+	//queryArtistConcerts(name: string): Promise<Observable<any> | undefined>
+	queryArtistAlbums(artistid: string): Promise<Observable<any> | undefined>
+	//queryAlbumTracks(albumid: string): Promise<Observable<any> | undefined>
+}
 
-  constructor() { }
+@Injectable({ providedIn: 'root' })
+export class QueryService implements IQueryService {
+	async queryArtist(name: string): Promise<Observable<any> | undefined> {
+		return this.spotify.request(`https://api.spotify.com/v1/search?type=artist&offset=0&limit=5&q=${name}`);
+	}
+
+	async queryArtistAlbums(artistid: string): Promise<Observable<any> | undefined> {
+		return this.spotify.request(`https://api.spotify.com/v1/artists/${artistid}/albums?market=US&limit=10&offset=0`);
+	}
+
+	constructor(
+		private spotify: SpotifyService
+	) { }
 }
