d44cc245e50ed6d2ba8e509ca39fbb0745df519b
Maxime Gagnon <m.gagnon941@gmail.com>
2022-10-10 11:06:55 -0400

    feat: Implement Spotify service
    Signed-off-by: 2156153 <m.gagnon941@gmail.com>

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-@Injectable({
-	providedIn: 'root'
-})
+
+@Injectable({ providedIn: "root" })
 export class SpotifyService {
+	authorized: boolean = false
 
-	constructor() { }
+	authorize(): void {
+		const body = new HttpParams().set("grant_type", "client_credentials");
+		const httpOptions = {
+			headers: new HttpHeaders({
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Authorization": "Basic " + btoa(this.CLIENT_ID + ":" + this.CLIENT_SECRET)
+			})
+		};
+
+		try {
+			this.http.post<any>("https://accounts.spotify.com/api/token", body.toString(), httpOptions)
+				.subscribe(response => this.token = response.access_token);
+		} catch (e) {
+			console.error(`Could not authorize ${this.CLIENT_ID} at Spotify.`)
+		}
+
+		this.authorized = true;
+		console.info(`Authorized ${this.CLIENT_ID} at Spotify.`);
+	}
+
+	async request(url: string): Promise<Observable<any> | undefined> {
+		if (!this.token) {
+			console.error("Client has not been authorized to Spotify!");
+			return undefined;
+		}
+
+		const httpOptions = {
+			headers: new HttpHeaders({
+				"Content-Type": "application/json",
+				"Authorization": "Bearer " + this.token
+			})
+		};
+
+		return this.http.get<any>(url, httpOptions);
+	}
+
+	private CLIENT_ID = "887f38697712471d8cbc1290c72147f8";
+	private CLIENT_SECRET = "9a6f963840b44c638eeb7d667afa64e2";
+	private token?: string
+
+	constructor(private http: HttpClient) { }
 }
