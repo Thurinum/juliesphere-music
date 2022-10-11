d0e2ab562b946faeedc3a817eb1dd27a10ae6670
Maxime Gagnon <m.gagnon941@gmail.com>
2022-10-11 11:23:35 -0400

    feat: Display album tracks
    Signed-off-by: 2156153 <m.gagnon941@gmail.com>

--- src/app/components/artist-details/artist-albums/artist-album-tracks/artist-album-tracks.component.ts
+++ src/app/components/artist-details/artist-albums/artist-album-tracks/artist-album-tracks.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HelperService } from 'src/app/services/helper.service';
+import { QueryService } from 'src/app/services/query.service';
 
 @Component({
 	selector: 'app-artist-album-tracks',
@@ -7,7 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistAlbumTracksComponent implements OnInit {
 	tracks: string[] = [];
+	subscription?: Subscription
 
-	constructor() { }
-	ngOnInit(): void {}
+	getTracks(albumid: string): void {
+		if (!albumid) {
+			this.helper.popup("No album id provided.");
+			return;
+		}
+
+		this.query.getAlbumTracks(albumid).then(
+			request => {
+				if (!request) {
+					this.helper.popup("Could not fetch album's tracks from Spotify.");
+					return;
+				}
+
+				request.subscribe(
+					response => {
+						if (!response || response.items.length === 0) {
+							this.helper.popup(`Album has no tracks.`);
+							return;
+						}
+
+						this.tracks = response.items.map((track: any) => track.name);
+					}
+				);
+			}
+		);
+	}
+
+	constructor(
+		private route: ActivatedRoute,
+		private query: QueryService,
+		private helper: HelperService
+	) { }
+
+	ngOnInit(): void {		
+		this.subscription = this.route.params.subscribe(params => {
+			this.getTracks(params["id"]);
+		});
+	}
+	ngOnDestroy(): void {
+		this.subscription?.unsubscribe();
+	}
 }
--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -6,7 +6,7 @@ interface IQueryService {
 	getArtist(name: string): Promise<Observable<any> | undefined>
 	//getArtistConcerts(name: string): Promise<Observable<any> | undefined>
 	getArtistAlbums(artistid: string): Promise<Observable<any> | undefined>
-	//getAlbumTracks(albumid: string): Promise<Observable<any> | undefined>
+	getAlbumTracks(albumid: string): Promise<Observable<any> | undefined>
 }
 
 @Injectable({ providedIn: 'root' })
@@ -17,6 +17,10 @@ export class QueryService implements IQueryService {
 
 	async getArtistAlbums(artistid: string): Promise<Observable<any> | undefined> {
 		return this.spotify.request(`https://api.spotify.com/v1/artists/${artistid}/albums?market=US&limit=10&offset=0`);
+	}
+
+	async getAlbumTracks(albumid: string): Promise<Observable<any> | undefined> {
+		return this.spotify.request(`https://api.spotify.com/v1/albums/${albumid}/tracks?market=US&limit=50&offset=0`);
 	}
 
 	constructor(
