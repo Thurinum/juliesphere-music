5d8fd6f48395e3f40d5da12e8f20ac77ce38f59c
Maxime Gagnon <m.gagnon941@gmail.com>
2022-10-12 12:47:28 -0400

    feat: Implement bandsintown service
    Signed-off-by: 2156153 <m.gagnon941@gmail.com>

--- src/app/services/bandsintown.service.ts
+++ src/app/services/bandsintown.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class BandsintownService {
+	API_KEY = "2b32475766802ac01eefda45e9e42ea0";
 
-  constructor() { }
+	async request(artist: string): Promise<Observable<any> | undefined> {
+		return this.http.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=${this.API_KEY}`);
+	}
+
+	constructor(
+		private http: HttpClient
+	) { }
 }
